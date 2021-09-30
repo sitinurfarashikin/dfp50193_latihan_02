0cf77ee37392f4af089e4666ebf1b075fdf26c4f
sitinurfarashikin <sitinurfarashikin@gmail.com>
2021-09-30 10:49:31 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ms">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Kedua</title>
+</head>
+<body>
+    <h1>Hello world </h1>
+    <?php
+    $nama = 'Siti Nur Farashikin';
+    $umur = 18 + 2 ;
+  
+    echo "$nama berumur $umur tahun ";
+    ?>
+
+</body>
+</html>
